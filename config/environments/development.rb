4b615e7e05a12dee6392250a720edb3025b86ad1
shawn ning <zxning@gmail.com>
2016-12-06 13:35:58 -0500

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,8 +25,8 @@ Caribou::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  
+
 	config.assets.debug = true
-	
-	config.serve_static_assets = true
+
+  config.serve_static_files = true
 end
